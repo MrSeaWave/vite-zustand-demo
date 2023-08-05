916de3d36fd191e60f8eaa4da84c08ad034f463e
MrSeaWave <MrDaemon@outlook.com>
2023-08-05 11:29:05 +0800

    chore: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { Dashboard } from './pages/Dashboard/Dashboard';
@@ -6,15 +7,23 @@ import { Home } from './pages/Home/Home';
 import { BearStorePage } from './pages/BearStorePage/BearStorePage';
 import ModalPage from './pages/ModalPage';
 
+type Props = {};
 function App() {
+  // let a = 1 === '1';
+  // let b=`${}`
+
+  // let c=2==='2]d'
+  const x = 'y';
+  const CONFIG = {};
+  // console.log('🚀 ~ file: App.tsx:13 ~ App ~ b:', b);
   return (
     <div className="App">
       <div className="app-container">
-        <Link to={'/'}>home</Link>
-        <Link to={'/dashboard'}>dashboard</Link>
-        <Link to={'/about'}>about</Link>
-        <Link to={'/bear-store'}>BearStorePage</Link>
-        <Link to={'/modal-page'}>Modal Page</Link>
+        <Link to="/">home</Link>
+        <Link to="/dashboard">dashboard</Link>
+        <Link to="/about">about</Link>
+        <Link to="/bear-store">BearStorePage</Link>
+        <Link to="/modal-page">Modal Page</Link>
       </div>
       <Routes>
         <Route index path="/" element={<Home />} />
